561de263968cc86568c69a218caf06d6ec4a1c01
Richard Roncancio <batusai513@msn.com>
2016-07-09 21:12:53 -0500

    add modal and more styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('\'Allo \'Allo!');
+$(() => {
+  $(".js-new-question").magnificPopup({
+    type: 'inline',
+    items: {
+      src: '#new-item-popup'
+    },
+    mainClass: 'mfp-zoom-in',
+    removalDelay: 150,
+    callbacks: {
+      open: function() {
+        var elements = this.content.find('.js-tags');
+        createTagElement(elements);
+      }
+    }
+  });
+
+  $(".js-response-editor").trumbowyg({
+    resetCss: true,
+    btns: [
+      ['viewHTML', 'bold', 'italic', 'unorderedList', 'orderedList'],
+      ['link', 'insertImage', 'fullscreen']
+    ]
+  });
+});
+
+function createTagElement(element){
+  return $(element).selectize({
+      plugins: ['remove_button'],
+      delimiter: ',',
+      persist: false,
+      create: function(input) {
+          return {
+              value: input,
+              text: input
+          }
+      }
+  });
+}
