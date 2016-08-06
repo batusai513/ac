0b0c1ee6724afc3e06601a0378b4fd02007b62f4
Richard Roncancio <batusai513@msn.com>
2016-08-06 13:05:22 -0500

    add carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,4 +39,12 @@ $(() => {
     ]
   });
 
+  $('.slider').slick({
+    slidesToShow: 3,
+    variableWidth: true,
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><span class="sr-only">Previous</span><svg class="sicon sicon-arrow-left"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="symbol-defs.svg#sicon-arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><span class="sr-only">Next</span><svg class="sicon sicon-arrow-right"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="symbol-defs.svg#sicon-arrow-right"></use></svg></button>'
+  })
+
 });
